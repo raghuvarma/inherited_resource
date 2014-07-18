ce5f04f163a625ef4af287e9a742857896421a18
Raghu Varma <raghuvarma@masymbol.com>
2014-07-18 15:47:49 +0530

    modified articles and add comments for articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,19 @@
 class ArticlesController < InheritedResources::Base
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  respond_to :html, :xml, :json
   
+  def create
+    create! { collection_url }
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def article_params
+      params.require(:article).permit(:name, :content)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718071537) do
+ActiveRecord::Schema.define(version: 20140718094703) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20140718071537) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
