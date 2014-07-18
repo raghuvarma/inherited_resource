b824324fd0451e75757070eb0d2af54212b8fd2b
Raghu Varma <raghuvarma@masymbol.com>
2014-07-18 22:26:28 +0530

    add inherited_resource in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,12 @@
-class CommentsController < ApplicationController
+class CommentsController < InheritedResources::Base
+  belongs_to :article
+  actions :create,  :destroy
+  
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    create! { collection_url }
   end
   
-  def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article)
-  end
- 
+  
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
